d262736e36d9489652b67581f662fbb358bc2b35
AndiswaMseleku <mselekuandiswa9@gmail.com>
2026-08-28 08:32:22 +0200

    Refactor portfolio data structure and comments

--- data/portfolio.js
+++ data/portfolio.js
@@ -340,7 +340,7 @@ const portfolioData = {
             name: "Introduction to Javascript Programming",
             organization: "Sololearn",
             date: "12 July 2023",
-            image: "assets/images/certificates/IntroductionjavaScript.png",
+            image: "assets/images/certificates/IntroductionJS.png",
             credentialUrl: "https://www.sololearn.com/certificates/CC-XGE5SXNX"
         },
         {
